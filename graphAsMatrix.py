0a4d0c24249f09821390f6f1f1f6f69555054c8d
Denilson Rodrigues <jundenilson@gmail.com>
2018-06-17 20:08:18 -0300

    Implementa representação de matriz e lê arquivo

---
+++ graphAsMatrix.py
@@ -0,0 +1,78 @@
+import re
+import numpy as np
+class Graph:
+  def __init__(self, vertices = []):
+    self.n = len(vertices)
+    self.vertices = vertices
+    self.matrix = np.zeros((self.n,self.n))
+
+  def getVertice(self,i):
+    if(i < len(self.vertices)):
+      return self.vertices[i]
+    return -1
+
+  def getIndexesAndOrder(self, v1Name, v2Name):
+    v1 = self.vertices.index(v1Name)
+    v2 = self.vertices.index(v2Name)
+
+    v1Index = v1 if v1 < v2 else v2
+    v2Index = v1 if v1 > v2 else v2
+    return [v1Index,v2Index]
+
+
+  def AddV(self, v1):
+    if v1 in self.vertices:
+      return False
+    self.vertices.append(v1)
+    self.matrix = np.append(self.matrix, np.zeros((self.n,1)), axis=1)
+    self.n += 1
+    self.matrix = np.append(self.matrix, np.zeros((1,self.n)), axis=0)
+    return True
+
+  def RemoveV(self, v1):
+    if v1 not in self.vertices:
+      return False
+
+    v1Index = self.vertices.index(v1)
+    self.vertices.remove(v1)
+    self.matrix = np.delete(self.matrix, v1Index, axis=1)
+    self.matrix = np.delete(self.matrix, v1Index, axis=0)
+    self.n -= 1
+    return True
+
+  def printMe(self):
+    if(len(self.matrix)):
+      for v in self.vertices:
+        index = int(v)-1
+        self.matrix[index][index] = 9
+    print(self.vertices)
+    print(self.matrix)
+
+  def AddE(self, v1, v2):
+    if(v1 not in self.vertices):
+      self.AddV(v1)
+    if(v2 not in self.vertices):
+      self.AddV(v2)
+    vertices = self.getIndexesAndOrder(v1,v2)
+    self.matrix[vertices[0]][vertices[1]] = 1
+
+  def RemoveE(self, v1, v2):
+    if(v1 not in self.vertices and v2 not in self.vertices):
+      return False
+
+    vertices = self.getIndexesAndOrder(v1,v2)
+    self.matrix[vertices[0]][vertices[1]] = 0
+
+  def getNeighborhood(self, v):
+    if( v not in self.vertices):
+      return False
+    vIndex = self.vertices.index(v)
+    neighbors = []
+    for i in range(vIndex,self.n):
+      if(self.matrix[vIndex][i] == 1):
+        neighbors.append(self.getVertice(i))
+
+    for i in range(vIndex,-1,-1):
+      if(self.matrix[i][vIndex] == 1):
+        neighbors.append(self.getVertice(i))
+    return neighbors
---
+++ main.py
@@ -0,0 +1,21 @@
+import graphAsMatrix as g
+
+def split(str):
+  return str.split("\t")
+file  = open("graph.txt","r")
+
+lines = int(split(file.readline())[1])
+
+print(lines)
+
+myGraph = g.Graph()
+
+for i in range(lines):
+  vertices = split(file.readline().rstrip())
+  v1 = vertices[0]
+  v2 = vertices[1]
+  print("adding ",v1," and ", v2)
+  myGraph.AddE(vertices[0],vertices[1])
+
+myGraph.printMe()
+myGraph.getNeighborhood('3')
